3d2e1754e57dfc30f9f2bab171f2d671037c06b1
Siddharth Thakkar <69584909+woustachemax@users.noreply.github.com>
2025-08-23 15:15:50 -0400

    Update GitHub.tsx

--- app/components/GitHub.tsx
+++ app/components/GitHub.tsx
@@ -6,12 +6,12 @@ export default function GitHubContributions() {
       <h2 className="text-3xl text-gray-500 font-bold mb-8">GitHub Activity</h2>
       
       <div className="grid gap-6">
-        <div className="bg-black/60 backdrop-blur-sm border border-blue-950 hover:borde-blue-400 rounded-xl p-6 hover:scale-[1.02] transition-all duration-300">
+        <div className="bg-black/60 backdrop-blur-sm border border-blue-950 rounded-xl p-6 hover:scale-[1.02] transition-all duration-300">
           <div className="flex justify-center">
             <img 
               src="https://ghchart.rshah.org/1e3a8a/woustachemax" 
               alt="GitHub Contributions" 
-              className="w-full max-w-4xl rounded-lg"
+              className="w-full max-w-4xl rounded-lg invert"
               loading="lazy"
             />
           </div>
